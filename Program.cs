eb603c0053516c1c038bd807367a4759413a040e
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-04-09 10:59:54 +0800

    hih

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Mylms
+{
+    public string Name;
+    public string Email;
+    public string Password;
+    public string Role;
+
+    public Mylms(string name, string email, string password, string role)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+    public void display_info()
+    {
+        Console.WriteLine($"Name: {Name}| Email: {Email}| Role: {Role}");
+    }
+}
+
+class Student : Mylms
+{
+    public string ClassName;
+
+    public Student(string name, string email, string password, string role, string className) : base(name, email, password, role)
+    {
+        ClassName = className;
+    }
+    public void SubmitAssignment() {
+        Console.WriteLine($"Student {Name} has submitted assignment");
+    }
+}
+class Teacher : Mylms
+{
+    public string Subject;
+
+    public Teacher(string name, string email, string password, string role, string subject) : base(name, email, password, role)
+    {
+        Subject = subject;
+    }
+    public void Grade()
+    {
+        Console.WriteLine($"Teacher {Name} has graded assignment");
+    }
+}
